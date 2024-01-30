81e74ed8f03f9f538d15a6975c32d7d80e76fe5a
musikamelabs <123078605+musikamelabs@users.noreply.github.com>
2024-01-30 18:43:38 +0100

    Update sound_event_detection.py

--- sound_event_detection.py
+++ sound_event_detection.py
@@ -1,3 +1,4 @@
+"""
 import numpy as np
 import pyaudio
 from matplotlib import pyplot as plt
@@ -66,3 +67,49 @@ if __name__ == "__main__":
     stream.stop_stream()
     stream.close()
     audio.terminate()
+"""
+import numpy as np
+from matplotlib import pyplot as plt
+from keras_yamnet import params
+from keras_yamnet.yamnet import YAMNet, class_names
+from keras_yamnet.preprocessing import preprocess_input
+from plot import Plotter
+
+# Set the path to your static audio file
+audio_file_path = 'demo_file.wav'
+
+################### SETTINGS ###################
+plt_classes = [0, 132, 420, 494]  # Speech, Music, Explosion, Silence
+class_labels = True
+FORMAT = np.float32
+CHANNELS = 1
+RATE = params.SAMPLE_RATE
+WIN_SIZE_SEC = 0.975
+CHUNK = int(WIN_SIZE_SEC * RATE)
+
+#################### MODEL #####################
+model = YAMNet(weights='keras_yamnet/yamnet.h5')
+yamnet_classes = class_names('keras_yamnet/yamnet_class_map.csv')
+
+#################### PROCESS AUDIO FILE ####################
+audio_data, _ = librosa.load(audio_file_path, sr=RATE, mono=True)
+n_frames = len(audio_data) // CHUNK
+
+if plt_classes is not None:
+    plt_classes_lab = yamnet_classes[plt_classes]
+    n_classes = len(plt_classes)
+else:
+    plt_classes = [k for k in range(len(yamnet_classes))]
+    plt_classes_lab = yamnet_classes if class_labels else None
+    n_classes = len(yamnet_classes)
+
+monitor = Plotter(n_classes=n_classes, FIG_SIZE=(12, 6), msd_labels=plt_classes_lab)
+
+for i in range(n_frames):
+    start = i * CHUNK
+    end = (i + 1) * CHUNK
+    data = preprocess_input(audio_data[start:end], RATE)
+    prediction = model.predict(np.expand_dims(data, 0))[0]
+    monitor(data.transpose(), np.expand_dims(prediction[plt_classes], -1))
+
+print("finished processing audio file")
